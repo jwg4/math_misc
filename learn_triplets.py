b9379263443ee1b46d5b10992047395da26c63d6
Jack Grahl <jack@prismfp.com>
2017-10-16 18:02:19 +0100

    Print the value for the number we entered.

--- learn_triplets.py
+++ learn_triplets.py
@@ -4,9 +4,12 @@ from helpers import make_lookup
 
 
 def check_triplet(number, name):
+    name = lookup[number]
     guess = input("%s?  " % name)
+    actual = lookup[str(guess)]
     if guess != int(number):
         print "No! %s is %s" % (name, number)
+        print "(%s is %s)" % (guess, actual)
         return False
     else:
         print ":)"
@@ -19,6 +22,6 @@ if __name__ == '__main__':
     c = 0
     while success:
         s = random.choice(lookup.keys())
-        success = check_triplet(s, lookup[s])
+        success = check_triplet(s, lookup)
         c = c + 1
     print "Streak of %d correct answers." % (c - 1, )
